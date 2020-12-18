ac90eae9b1b25528e1026d13b2895ea99f785610
henrikesilva <henrikelima.0502@gmail.com>
2020-12-18 14:23:18 -0300

    Inclusão Swagger

--- src/API.Application/Startup.cs
+++ src/API.Application/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace application
 {
@@ -30,6 +31,26 @@ namespace application
             ConfigureRepository.ConfigureDependenciesRepository(services);
 
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Curso de API com AspNetCore 3.1",
+                    Description = "Arquitetura DDD",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Henrique Lima",
+                        Email = "henrique.lima25@aluno.unip.br"
+                    },
+
+                    License = new OpenApiLicense
+                    {
+                        Name = "Termo de Licença de Uso"
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +60,13 @@ namespace application
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Curso de API com AspNetCore 3.1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
